da67b5da676b17181fd6bdbe671a8691c7bb2e61
Sebastian Price <skphoopa@yahoo.com>
2018-11-13 12:02:48 -0500

    Python Notes

---
+++ python101.py
@@ -0,0 +1,29 @@
+print ("Hello, New World")
+
+print ("""
+It was a bright and sunny day,
+And a bird shit in me eye.
+Yarrrgh.
+""")
+
+# Variables
+# - Strings, letters, numbers, or any other stuff you can make with a keyboard.
+# - A variable is just a fast way to refer to something else.
+# - They don't make the program faster. They make it slower.
+# - They just make it easier for us to write programs.
+
+# theBestClass = "the 11-18 immersive"
+# print (theBestClass)
+
+#Data Types
+# - Programming languages see different types of variables differently.
+# - String: English characters.
+# - Numbers: Numerals.
+# - Floats & Integers
+# -- Floats have decimal points. Integers do not!
+# - Booleans: True/False, On/Off, 1/0
+# - List: A single variable with a bunch of parts (A.K.A. an array)
+# - Dictionary: Variable of variables
+# - Object: Super dictionaries
+
+# - Primitive Data Types: String, Number, Boolean
