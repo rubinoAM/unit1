bb72470b2857dcc39fb58fa6792ddace492305c3
Sebastian Price <skphoopa@yahoo.com>
2018-11-13 14:26:41 -0500

    Some other notes

--- python101.py
+++ python101.py
@@ -1,41 +1,74 @@
-print ("Hello, New World")
+# print ("Hello, New World")
 
-print ("""
-It was a bright and sunny day,
-And a bird pooped in me eye.
-Yarrrgh.
-""")
+# print ("""
+# It was a bright and sunny day,
+# And a bird pooped in me eye.
+# Yarrrgh.
+# """)
 
-# Variables
-# - Strings, letters, numbers, or any other stuff you can make with a keyboard.
-# - A variable is just a fast way to refer to something else.
-# - They don't make the program faster. They make it slower.
-# - They just make it easier for us to write programs.
+# # Variables
+# # - Strings, letters, numbers, or any other stuff you can make with a keyboard.
+# # - A variable is just a fast way to refer to something else.
+# # - They don't make the program faster. They make it slower.
+# # - They just make it easier for us to write programs.
 
-# theBestClass = "the 11-18 immersive"
-# print (theBestClass)
+# # theBestClass = "the 11-18 immersive"
+# # print (theBestClass)
 
-#Data Types
-# - Programming languages see different types of variables differently.
-# - String: English characters.
-# - Numbers: Numerals.
-# - Floats & Integers
-# -- Floats have decimal points. Integers do not!
-# - Booleans: True/False, On/Off, 1/0
-# - List: A single variable with a bunch of parts (A.K.A. an array)
-# - Dictionary: Variable of variables
-# - Object: Super dictionaries
+# #Data Types
+# # - Programming languages see different types of variables differently.
+# # - String: English characters.
+# # - Numbers: Numerals.
+# # - Floats & Integers
+# # -- Floats have decimal points. Integers do not!
+# # - Booleans: True/False, On/Off, 1/0
+# # - List: A single variable with a bunch of parts (A.K.A. an array)
+# # - Dictionary: Variable of variables
+# # - Object: Super dictionaries
 
-# - Primitive Data Types: String, Number, Boolean
-month = "November";
-print (type(month))
-date = 13
-print (type(date))
-dateAsFloat = 13.0
-print (type(dateAsFloat))
-aBool = True #True and False need to be uppercase!!!
-print (type(aBool))
-aList = []
-print (type(aList))
-aDict = {}
-print (type(aDict))
+# # - Primitive Data Types: String, Number, Boolean
+# month = "November";
+# print (type(month))
+# date = 13
+# print (type(date))
+# dateAsFloat = 13.0
+# print (type(dateAsFloat))
+# aBool = True #True and False need to be uppercase!!!
+# print (type(aBool))
+# aList = []
+# print (type(aList))
+# aDict = {}
+# print (type(aDict))
+
+# first = "Michael"
+# last = "Rubino"
+# fullName = first + "" + last
+# print (fullName)
+
+# fourteen = 10 + 4
+# print (fourteen)
+# fourteen = "10" + "4"
+# print (fourteen)
+# #fourteen = 10 + "4"
+# #print (fourteen)
+
+# #cast = change a variable to a new data type
+# fourteen = int("10") + 4
+# print (fourteen)
+
+# Math = +, -, /, *, %
+print (2+2)
+print (2-2)
+print (2/2)
+print (2*2)
+# % = Modulus, which divides the number and gives you the remainder
+print (2%2)
+print (2**3)
+print (87**10)
+# A string and a * and a number = Give me x amount of strings
+print ("-" * 20)
+
+# Python doesn't have a simple incrementer
+num = 1
+num += 1
+print (num)
